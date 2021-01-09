b52b41c1ac4f4e44e93b533679b377211609d711
Riley Costello <rileytcostello@gmail.com>
2021-01-09 10:59:05 -0700

    initial

---
+++ config/connection.js
@@ -0,0 +1,31 @@
+//require mysql
+var mysql = require('mysql');
+
+
+var connection;
+//start of connection to mysql DB and JawsDb
+if (process.env.JAWSDB_URL) {
+	// Jaws
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: 'Milagro12090',
+		database: 'burgers_db'
+	})
+};
+
+// Make the connection to MySQL
+connection.connect(function(err) {
+  if (err) {
+    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+    return;
+  }
+  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+});
+
+// Export connection for ORM use
+module.exports = connection;
